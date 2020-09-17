84d3e29a9372a6124e3f7d42407bd618699797e5
rajesh125 <47712439+rajesh125@users.noreply.github.com>
2020-09-17 12:09:38 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,5 +1,5 @@
 def call(String hubuser, String repo, String repotag) {
 
-    sh "docker pull $(hubuser)/${repo}:${repotag}-v"${env.BUILD_NUMBER}""
-    sh "docker run -d $(hubuser)/${repo}:${repotag}-v"${env.BUILD_NUMBER}""
+    sh "docker pull $(hubuser)/${repo}:${repotag}-v'${env.BUILD_NUMBER}'"
+    sh "docker run -d $(hubuser)/${repo}:${repotag}-v'${env.BUILD_NUMBER}'"
 }
