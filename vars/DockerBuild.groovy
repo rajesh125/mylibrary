00402c543bd086d8cc9c9fd9969cd248317ee906
rajesh125 <47712439+rajesh125@users.noreply.github.com>
2020-09-17 13:30:36 +0530

    Update DockerBuild.groovy

--- vars/DockerBuild.groovy
+++ vars/DockerBuild.groovy
@@ -1,7 +1,7 @@
-def call(String hubuser, String repo, String repotag) {
+def call(String hubuser, String repo) {
 
     sh "docker login -u '$Username' -p '$Password'"
-    sh "docker image build -t ${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER} ."
-    sh "docker image push ${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"
-    sh "docker image rm ${hubuser}/${repo}:${repotag}-v${env.BUILD_NUMBER}"   
+    sh "docker image build -t ${hubuser}/${repo}:v${env.BUILD_NUMBER} ."
+    sh "docker image push ${hubuser}/${repo}:v${env.BUILD_NUMBER}"
+    sh "docker image rm ${hubuser}/${repo}:v${env.BUILD_NUMBER}"   
 }
