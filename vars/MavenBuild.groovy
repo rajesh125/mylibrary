3151f2c3994e4930194637048a60a522aa4a5f27
rajesh125 <47712439+rajesh125@users.noreply.github.com>
2020-09-21 14:37:12 +0530

    Update MavenBuild.groovy

--- vars/MavenBuild.groovy
+++ vars/MavenBuild.groovy
@@ -1,4 +1,4 @@
 def call() {
       sh "mvn clean install -Dmaven.test.skip=true"
-      openTasks canComputeNew: false, defaultEncoding: '', excludePattern: '', healthy: '', high: '', low: '', normal: '', pattern: '**/src/*.java', unHealthy: ''
+      openTasks canComputeNew: false, defaultEncoding: '', excludePattern: '', healthy: '', high: '', low: '', normal: '', pattern: '**src/test/java/de/robertdey/kitchenbob/*.java', unHealthy: ''
       }
